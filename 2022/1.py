8cebdfc475a519feb96faaa86ef376eff21e1b7d
M. Thomas <m.thomas@gso.schule.koeln>
2022-12-01 19:07:48 +0100

    input wird zeilenweise eingelesen

---
+++ 2022/1.py
@@ -0,0 +1,16 @@
+# https://adventofcode.com/2022/day/1
+# Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
+
+# Read Lines from file
+with open('1_input.txt') as input_file:
+    for line in input_file:
+        if line == '\n':
+            print('blank\n', end = '')
+        else:
+            print(line, end='')
+
+
+# add up until blank line 
+
+
+# find largest sum
