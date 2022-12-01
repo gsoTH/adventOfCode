239f44a44c7f62e863bd66f8e8ca3b7af1de241c
M. Thomas <m.thomas@gso.schule.koeln>
2022-12-01 19:18:15 +0100

    Klasse für Elfen erzeugt um die Verwaltung der Daten einfacher zu machen

--- 2022/1.py
+++ 2022/1.py
@@ -1,8 +1,20 @@
 # https://adventofcode.com/2022/day/1
 # Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
 
+class Elf:
+    def __init__(self, number):
+        self.number = number
+        self.calories = 0
+
+    def add(calories):
+        self.calories = self.calories + calories
+
+
+allElfes = []
+
 # Read Lines from file
 with open('1_input.txt') as input_file:
+    
     for line in input_file:
         if line == '\n':
             print('blank\n', end = '')
@@ -14,3 +26,5 @@ with open('1_input.txt') as input_file:
 
 
 # find largest sum
+
+
