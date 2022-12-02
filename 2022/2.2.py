e78e4e3d14acbe7e44a49c8b149b1500281903c4
M.Thomas <m.thomas@gso.schule.koeln>
2022-12-02 10:35:49 +0100

    Funktioniert mit BspInput

--- 2022/2.2.py
+++ 2022/2.2.py
@@ -6,6 +6,11 @@
 # X means you need to lose, 
 # Y means you need to end the round in a draw, and 
 # Z means you need to win. Good luck!"
+
+# Points:
+#   1 for Rock, 2 for Paper, and 3 for Scissors
+#   0 if you lost, 3 if the round was a draw, and 6 if you won
+
 sum = 0
 
 with open('2_input.txt') as input_file:
@@ -21,44 +26,44 @@ with open('2_input.txt') as input_file:
                 print('Scissors')
         match chars[1]:
             case 'X':
-                print('Rock')
-                sum = sum + 1
+                print('lose')
+                sum = sum + 0
                 match chars[0]:
                     case 'A':
-                        print('draw')
+                        print('Scissors')
                         sum = sum + 3
                     case 'B': 
-                        print('lost')
-                        sum = sum + 0
+                        print('Rock')
+                        sum = sum + 1
                     case 'C':
-                        print('won')
-                        sum = sum + 6
+                        print('Paper')
+                        sum = sum + 2
             case 'Y': 
-                print('Paper')
-                sum = sum + 2
-                match chars[0]:
-                    case 'A':
-                        print('won')
-                        sum = sum + 6
-                    case 'B': 
-                        print('draw')
-                        sum = sum + 3
-                    case 'C':
-                        print('lost')
-                        sum = sum + 0
-            case 'Z':
-                print('Scissors')
+                print('draw')
                 sum = sum + 3
                 match chars[0]:
                     case 'A':
-                        print('lost')
-                        sum = sum + 0
+                        print('Rock')
+                        sum = sum + 1
                     case 'B': 
-                        print('won')
-                        sum = sum + 6
-                    case 'C':
-                        print('draw')
+                        print('Scissors')
                         sum = sum + 3
+                    case 'C':
+                        print('Paper')
+                        sum = sum + 2
+            case 'Z':
+                print('win')
+                sum = sum + 6
+                match chars[0]:
+                    case 'A':
+                        print('Paper')
+                        sum = sum + 2
+                    case 'B': 
+                        print('Scissors')
+                        sum = sum + 3
+                    case 'C':
+                        print('Rock')
+                        sum = sum + 1
 
 print(sum)
 
