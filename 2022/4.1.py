8967f5e4416493248702549ce9f5092f94cbfd6e
M. Thomas <m.thomas@gso.schule.koeln>
2022-12-05 12:03:37 +0100

    Ausgangslage und Planung

---
+++ 2022/4.1.py
@@ -0,0 +1,9 @@
+# https://adventofcode.com/2022/day/4
+
+# In how many assignment pairs does one range fully contain the other?
+
+#Zeilen einlesen
+    #Spalten aufteilen
+    #Wertebereiche finden
+        #Prüfen ob linke Seite die rechte Seite vollkommen abdeckt
+        #Prüfen ob umgekehrt 
