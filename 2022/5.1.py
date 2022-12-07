73cd667483fcb22afc07abaf332d2c11018991c2
M. Thomas <m.thomas@gso.schule.koeln>
2022-12-07 14:43:19 +0100

    Erster Teil erfolgreich

--- 2022/5.1.py
+++ 2022/5.1.py
@@ -8,12 +8,22 @@ __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 
-def PrintStacks(stacksToPrint):
-    for y in range(0,len(stacksToPrint[0])):
-        for x in range(0,len(stacksToPrint)):
-            print(stacksToPrint[x][y], end = '')
-        print('')
+def PrintStacks():
+    for x in range(0,len(stacks)):
+        print(x, ': ', end = '')
+        for y in range(0,len(stacks[x])):
         
+            print(stacks[x][y], end = '')
+        print('')
+    print('-------------------------------------')
+
+def PrintTopLayer():
+    text = ''
+    for x in range(0,len(stacks)):
+        text = text + stacks[x][0]
+
+    print('Top Layers: ', text)
+            
 
 
 #     [D]    
@@ -27,34 +37,45 @@ def PrintStacks(stacksToPrint):
 # move 1 from 1 to 2
 
 
-
+# Stacks einlesen
 stacks = [[],[],[]]
-
 with open(os.path.join(__location__, '5_input_stacks.txt')) as stack_input_file:
     for line in stack_input_file:
         
-        stackLimit = 3
-        if len(line) == 36:                                           # True for puzzle input
-            stackLimit = 9
+        stackLimit = 9
+        if len(line) < 30:                                           # True for test input
+            stackLimit = 3
+        while len(stacks) < stackLimit:
+            stacks.append([])
+
         for i in range(0, stackLimit):
-            stacks[i].append(line[1+(i*4)])
+            item = line[1+(i*4)]
+            if not item == ' ':
+                stacks[i].append(item)
 
         
-
+# Nummern entfernen
 for stack in stacks:
-    del stack[-1]                                                     # Nummern entfernen
-PrintStacks(stacks)
-            
+    del stack[-1]                                                     
+PrintStacks()
+
+
+# moves einlesen
 moves = []
 with open(os.path.join(__location__, '5_input_moves.txt')) as moves_input_file:
     for line in moves_input_file:
 
 
-        result = re.search('move (\d+) from (\d+) to (\d+)', line)
-        amount = result.group(1)
-        fromStack = result.group(2)
-        toStack = result.group(3)
+        result = re.search('move (\d+) from (\d+) to (\d+)', line)      #https://regex101.com/r/CE1LMf/1
+        amount = int(result.group(1))
+        fromStack = int(result.group(2)) - 1
+        toStack = int(result.group(3)) - 1
 
+        print(amount, fromStack, toStack)
 
-        #print(amount, fromStack, toStack)
-         
+        for i in range(0, amount):
+            stacks[toStack].insert(0, stacks[fromStack][0])
+            del stacks[fromStack][0]
+        PrintStacks()
+
+PrintTopLayer()
